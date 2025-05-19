b421e414d15412b30ed2ab3e8a7160b3c8c99061
atharrva01 <atharvaborade568@gmail.com>
2025-05-20 00:24:36 +0530

    Update and rename Day_1_Print.py to Week1/Daily-Learnings/Day_1_Print.py

--- Day_1_Print.py
+++
@@ -1,3 +0,0 @@
-#In the very 1st day i learnt about print function.
-
-print("Hello World This is Atharva and this is my 1st Python program")
---
+++ Week1/Daily-Learnings/Day_1_Print.py
@@ -0,0 +1,3 @@
+#In the very 1st day i learnt about print function.
+
+print("Hello World This is Atharva and this is my 1st Python program")
